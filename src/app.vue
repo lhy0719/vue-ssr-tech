2e76e30d7051566c32784188be8e5d07beef86cc
xuhaitao <xuhaitao@jd.com>
2018-04-09 11:11:24 +0800

    删除临时文件

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="test">{{text}}</div>
+  <div id="app">
+      <div id="cover"></div>
+  </div>
 </template>
 
 <script>
+import Header from './todo/header.vue'
 export default {
   data(){
       return {
@@ -12,9 +15,24 @@ export default {
 }
 </script>
 
-<style>
-#test{
-    color: red;
+<style lang="stylus" scoped>
+#app{
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    
+}
+#cover{
+    position absolute
+    left 0
+    top 0
+    right 0
+    bottom 0
+    background-color #999
+    opacity .9
+    z-index -1
 }
 </style>
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ const config = {
                 loader:'vue-loader'
             },
             {
+                test:/\.jsx$/,
+                loader:'babel-loader'
+            },
+            {
                 test:/\.css$/,
                 use:[
                     'style-loader',
@@ -27,6 +31,12 @@ const config = {
                 use:[
                     'style-loader',
                     'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            sourceMap:true
+                        }
+                    },
                     'stylus-loader'
                 ]
             },
